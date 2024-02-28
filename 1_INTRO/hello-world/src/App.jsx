53e4ffd108f2772530502c716a970cf0590162ff
Ogawa Kei <keisiotane@hotmail.com>
2024-02-27 21:03:35 -0300

    update curso react

--- 1_INTRO/hello-world/src/App.jsx
+++ 1_INTRO/hello-world/src/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
       <h2>Emmet funcionou</h2>
+      <h3>Hello World novamente para dar sorte</h3>
     </div>
   );
 }
