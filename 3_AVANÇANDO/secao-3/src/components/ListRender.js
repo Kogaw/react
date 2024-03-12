981a42e9da84fc12eca9543c8523402a9455ae5d
Ogawa Kei <keisiotane@hotmail.com>
2024-03-11 21:40:10 -0300

    update react

--- 3_AVANÇANDO/secao-3/src/components/ListRender.js
+++ 3_AVANÇANDO/secao-3/src/components/ListRender.js
@@ -3,6 +3,12 @@ import { useState } from "react"
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias"])
 
+    const [users] = useState([
+      {id: 1, name: "Matheus", age:31},
+      {id: 72342141, name:"Pedro", age:28},
+      {id: 18231321, name:"João", age: 44 }
+    ])
+
     
   return (
     <div>
@@ -11,6 +17,14 @@ const ListRender = () => {
                 <li key={i}>{item}</li>
             ))}
         </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.age}
+            </li>
+          ))}
+        </ul>
+        <button>Delete random user</button>
     </div>
   )
 }
