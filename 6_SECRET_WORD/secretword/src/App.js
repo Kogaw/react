144827f204488fe69c8041cf5605605fe04a8c99
Ogawa Kei <keisiotane@hotmail.com>
2024-03-19 20:24:27 -0300

    update react

---
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -0,0 +1,45 @@
+// CSS
+import './App.css';
+
+// React
+import{ useCallback, useEffect, useState } from 'react'
+
+// data
+import {wordsList} from "./data/words"
+
+// components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id:1, name:"start"},
+  {id:2, name:"game"},
+  {id:3, name:"end"}
+]
+
+function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  //starts the secret word
+const startGame = () => {
+  setGameStage(stages[1].name)
+}
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[3].name)
+  }
+
+  return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver />}    
+    </div>
+  );
+}
+
+export default App;
---
+++ 6_SECRET_WORD/secretword/src/components/Game.js
@@ -0,0 +1,12 @@
+import styles from './Game.css'
+
+const Game = (verifyLetter) => {
+  return (
+    <div>
+            <button></button>
+    </div>
+
+  )
+}
+
+export default Game
